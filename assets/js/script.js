a56c8ae0dc4e90a04212f55d5afbdec36ca492b7
Tapon chandra sarker <108454446+taponbiswas007@users.noreply.github.com>
2024-07-01 03:14:08 +0600

    lsdfjslfjls

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
     // Set the date and time to count down to
     var targetDate = new Date('July 10, 2024 01:34:00');
 
@@ -24,4 +24,30 @@ $(document).ready(function() {
 
     updateCountdown();
     var x = setInterval(updateCountdown, 1000);
+
+    // Add the .active-day class to the current day
+    function setActiveDay() {
+        var currentDayIndex = new Date().getDay() - 1; // JavaScript's getDay() returns 0 for Sunday, 1 for Monday, etc.
+        // Adjust for your needs if Sunday should be considered the start of the week
+        if (currentDayIndex < 0) {
+            currentDayIndex = 6; // Saturday
+        }
+        $('.day-list-item').removeClass('active-day');
+        $('.day-list-item').eq(currentDayIndex).addClass('active-day');
+    }
+
+    // Initially set the active day
+    setActiveDay();
+
+    // Handle button click event
+    $('.coinrreceived-btn').click(function () {
+        var parent = $(this).closest('.day-list-item');
+        if (parent.hasClass('active-day')) {
+            parent.removeClass('active-day').addClass('coin-received');
+        }
+    });
+
+    $(".get-awerdnow").click(function () {
+        $(".get-awerd-popup").toggle();
+    });
 });
