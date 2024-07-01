b5f3351826c4c8240fdc6798a39f9079da1fa027
Tapon chandra sarker <108454446+taponbiswas007@users.noreply.github.com>
2024-07-02 00:55:29 +0600

    lfjsajf

--- assets/js/script.js
+++ assets/js/script.js
@@ -122,6 +122,41 @@ $(document).ready(function () {
         $(".idlequestlist").slideToggle();
     });
 
-
+    $('heading-line').slick({
+        dots: false,
+        infinite: false,
+        speed: 300,
+        arrows: false,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        responsive: [
+          {
+            breakpoint: 1920,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
+      });
 
 });
