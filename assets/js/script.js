eb3ce8a4ff3e6c63e5aa19eedbb7680579acd9b8
taponbiswas007 <taponbiswassdp@gmail.com>
2024-07-01 15:01:44 +0600

    klfjlsajf

--- assets/js/script.js
+++ assets/js/script.js
@@ -121,5 +121,43 @@ $(document).ready(function () {
     
         $(".idlequestlist").slideToggle();
     });
+    $('.responsive').slick({
+        dots: false, // Ensure dots are disabled
+        infinite: false,
+        arrows: false, // Ensure arrows are disabled
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                   
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                   
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                   
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    });
+    
     
 });
